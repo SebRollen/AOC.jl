5f2c65d30cbd075e931435cfb54e37104a3e2a8e
Sebastian Rollen <sebastian.rollen@betterment.com>
2019-11-30 22:57:16 -0500

    Prepare for 2019 day 1

---
+++ src/2019/day_1.jl
@@ -0,0 +1,14 @@
+# https://adventofcode.com/2019/day/1
+using AdventOfCode
+
+input = readlines("data/2019/day_1.txt")
+
+function part_1(input)
+    nothing
+end
+@info part_1(input)
+
+function part_2(input)
+    nothing
+end
+@info part_2(input)
